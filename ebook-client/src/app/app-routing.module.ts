d278dae102ae8ad582b3b97f97509cae06956bc6
Horst Boehse <daniel123@bossmail.de>
2020-10-17 18:37:00 +0200

    Added routing information for ebooks

--- ebook-client/src/app/app-routing.module.ts
+++ ebook-client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EbookComponent } from './ebook/ebook.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: 'ebooks', component: EbookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
